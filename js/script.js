6b7c9cdc723d0a91f5c8bac104caf12fcc834b67
sandrofajar <55909488+sandrofajar@users.noreply.github.com>
2019-09-28 14:13:59 +0700

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,85 @@
+function searchData() {
+    $('#data-list').html(`
+	  <div class="d-flex justify-content-center">
+		<div class="spinner-border text-danger" role="status">
+			<span class="sr-only">Loading...</span>
+		</div>
+	  </div>
+		`);
+    $.ajax({
+        //url: 'https://smk-smeatdkosgoro2pdg.sch.id/lulusapikgr/lulus.php',
+		url: 'http://localhost/kelulusan/lulusapi/lulus.php',
+        type: 'get',
+        dataType: 'json',
+		//responseType:'application/json',
+        data: {
+            'id' : $('#search-input').val()
+        },
+        success: function(result){
+				console.log(result);
+
+            if(result.status == "sukses"){
+                let lulus = result.data;
+                $.each(lulus, function(i, data){
+					console.log(result.status);
+					if(data.ket == "LULUS") {
+						$('#data-list').html(`
+						    <div class="row mt-3 justify-content-center">
+								<table border=0 width=100%>
+									<tr><td>No. Ujian </td><td>:<b> ` + data.no + `</td></tr>
+									<tr><td>Nama Siswa </td><td>:<b> ` + data.nama + `</td></tr>
+									<tr><td>Sekolah Asal </td><td>:<b> ` + data.sekolah + `</td></tr>
+								</table><br><br><p><p>
+								<table width=100%>
+									<tr><td><h4><center>&nbsp;</h4></td></tr>
+								</table>
+								<table width=100%>
+									<tr><td><h4><center>Anda Dinyatakan :</h4></td></tr>
+									<tr><td><h3><b><font color=blue><center>` + data.ket + `</h3></td></tr>
+								</table>
+							</div>
+                    `) }
+					if(data.ket == "TIDAK LULUS") {
+						$('#data-list').html(`
+						    <div class="row mt-3 justify-content-center">
+								<table border=0 width=100%>
+									<tr><td>No. Ujian </td><td>:<b> ` + data.no + `</td></tr>
+									<tr><td>Nama Siswa </td><td>:<b> ` + data.nama + `</td></tr>
+									<tr><td>Sekolah Asal </td><td>:<b> ` + data.sekolah + `</td></tr>
+								</table><br><br><p><p>
+								<table width=100%>
+									<tr><td><h4><center>&nbsp;</h4></td></tr>
+								</table>
+								<table width=100%>
+									<tr><td><h4><center>Anda Dinyatakan :</h4></td></tr>
+									<tr><td><h3><b><font color=red><center>` + data.ket + `</h3></td></tr>
+								</table>
+							</div>
+                    `) }
+                });
+
+                $('#search-input').val('');
+
+            }
+            if(result.status == "gagal"){
+                $('#data-list').html(`
+                    <div class="col">
+                        <h3 class="text-center"><font color=red>Data Tidak Ditemukan</font></h3>
+                    </div>
+                `)
+            }
+        }
+    });
+}
+
+$('#search-button').on('click', function(){
+	    searchData();    
+});
+
+$('#search-input').on('keyup', function (e){
+    if(e.keyCode === 13)
+    {
+        searchData();
+    }
+});
+
